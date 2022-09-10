b2a1e453208836d55f046f8e63bf224e29bdbc9a
kum-wh <wingho32@gmail.com>
2022-09-11 05:00:00 +0800

    Update Model for better accuracy
    Change CNN and ANN and change to only 5 outputs

--- AI.py
+++ AI.py
@@ -2,46 +2,49 @@ import numpy as np
 import tensorflow as tf
 
 
-EPOCHS = 5
+EPOCHS = 20
 
 
 #Main function that compiles the loading of the data and training of the model.
 def main():
 
-    x_train, extracted_x_train = load_data()
-    y_train = load_label()
-    
+    acc_x, acc_y, acc_z, gyr_x, gyr_y, gyr_z, x_train, y_train = load_data()
+
     # Get a compiled neural network
     model = get_ann_model()
-    print(model.summary())
 
     # Fit model on training data
-    model.fit(x_train, y_train, epochs=EPOCHS)
+    model.fit((acc_x, acc_y, acc_z, gyr_x, gyr_y, gyr_z, x_train), y_train, epochs=EPOCHS)
     
-    x_test, extracted_x_test = load_test_data()
-    y_test = load_test_label()
+    t_acc_x, t_acc_y, t_acc_z, t_gyr_x, t_gyr_y, t_gyr_z, x_test, y_test = load_test_data()
     
     # Evaluate neural network performance
-    model.evaluate(x_test,  y_test, verbose=2)
-    
-    model = get_ann_model_for_extracted_data()
-    print(model.summary())
-    model.fit(extracted_x_train, y_train, epochs=EPOCHS)
-    model.evaluate(extracted_x_test,  y_test, verbose=2)
+    model.evaluate((t_acc_x, t_acc_y, t_acc_z, t_gyr_x, t_gyr_y, t_gyr_z, x_test),  y_test, verbose=2)
 
+    # Create confusion matrix
+    y_pred = model.predict((t_acc_x, t_acc_y, t_acc_z,t_gyr_x, t_gyr_y, t_gyr_z, x_test))
+    y_pred = tf.argmax(y_pred, axis=1)
+    y_test = tf.argmax(y_test, axis=1 )
+    print(tf.math.confusion_matrix(y_test, y_pred))
+    '''
     model = get_cnn_model()
-    print(model.summary())
-    model.fit(x_train, y_train, epochs=EPOCHS)
-    model.evaluate(x_test,  y_test, verbose=2)
+    model.fit((acc_x, acc_y, acc_z, gyr_x, gyr_y, gyr_z, x_train), y_train, epochs=EPOCHS)
+    model.evaluate((t_acc_x, t_acc_y, t_acc_z, t_gyr_x, t_gyr_y, t_gyr_z, x_test),  y_test, verbose=2)
+    y_pred = model.predict((t_acc_x, t_acc_y, t_acc_z,t_gyr_x, t_gyr_y, t_gyr_z, x_test))
+    y_pred = tf.argmax(y_pred, axis=1)
+    y_test = tf.argmax(y_test, axis=1 )
+    print(tf.math.confusion_matrix(y_test, y_pred))
+    '''
 
+    '''
     model = get_rnn_model()
     print(model.summary())
     model.fit(x_train, y_train, epochs=EPOCHS)
     model.evaluate(x_test,  y_test, verbose=2)
-
+    
     # Extracts the model weights and bias.
     # write_param(model)
-
+    '''
 
 # Function loads the model layer parameters into a text file.
 def write_param(model):
@@ -118,14 +121,35 @@ def load_data():
         for row in f:
             body_gyr_z.append(row.replace("  ", " ").strip().split(" "))
 
-    body_acc_x = np.array(body_acc_x).astype(np.float64)
-    body_acc_y = np.array(body_acc_y).astype(np.float64)
-    body_acc_z = np.array(body_acc_z).astype(np.float64)
-    body_gyr_x = np.array(body_gyr_x).astype(np.float64)
-    body_gyr_y = np.array(body_gyr_y).astype(np.float64)
-    body_gyr_z = np.array(body_gyr_z).astype(np.float64)
+    print("Loading Training Labels . . .")
 
-    return np.transpose([body_acc_x, body_acc_y, body_acc_z, body_gyr_x, body_gyr_y, body_gyr_z],(1,2,0)), extract_data(body_acc_x, body_acc_y, body_acc_z, body_gyr_x, body_gyr_y, body_gyr_z)
+    label = []
+
+    with open("Dataset\\train\\y_train.txt") as f:
+
+        for row in f:
+            number = int(row.strip())
+
+            if number == 1:
+                label.append([1,0,0,0,0])
+            elif number == 2:
+                label.append([0,1,0,0,0])
+            elif number == 3:
+                label.append([0,0,1,0,0])
+            elif number == 4:
+                label.append([0,0,0,1,0])
+            else:
+                label.append([0,0,0,0,1])
+
+    total = np.transpose([body_acc_x, body_acc_y, body_acc_z, body_gyr_x, body_gyr_y, body_gyr_z],(1,2,0)).astype(np.float64)
+    body_acc_x = np.transpose([body_acc_x],(1,2,0)).astype(np.float64)
+    body_acc_y = np.transpose([body_acc_y],(1,2,0)).astype(np.float64)
+    body_acc_z = np.transpose([body_acc_z],(1,2,0)).astype(np.float64)
+    body_gyr_x = np.transpose([body_gyr_x],(1,2,0)).astype(np.float64)
+    body_gyr_y = np.transpose([body_gyr_y],(1,2,0)).astype(np.float64)
+    body_gyr_z = np.transpose([body_gyr_z],(1,2,0)).astype(np.float64)
+
+    return body_acc_x, body_acc_y, body_acc_z, body_gyr_x, body_gyr_y, body_gyr_z, total, np.array(label).astype(np.float64)
 
 
 # Function loads the sensor data for testing.
@@ -164,68 +188,39 @@ def load_test_data():
         for row in f:
             body_gyr_z.append(row.replace("  ", " ").strip().split(" "))
 
-    body_acc_x = np.array(body_acc_x).astype(np.float64)
-    body_acc_y = np.array(body_acc_y).astype(np.float64)
-    body_acc_z = np.array(body_acc_z).astype(np.float64)
-    body_gyr_x = np.array(body_gyr_x).astype(np.float64)
-    body_gyr_y = np.array(body_gyr_y).astype(np.float64)
-    body_gyr_z = np.array(body_gyr_z).astype(np.float64)
-
-    return np.transpose([body_acc_x, body_acc_y, body_acc_z, body_gyr_x, body_gyr_y, body_gyr_z],(1,2,0)).astype(np.float64), extract_data(body_acc_x, body_acc_y, body_acc_z, body_gyr_x, body_gyr_y, body_gyr_z)
-
-
-# Function loads the label actions respective to the sensor data.
-def load_label():
-
-    print("Loading Training Labels . . .")
-
-    label = []
-
-    with open("Dataset\\train\\y_train.txt") as f:
-        for row in f:
-            number = int(row.strip())
-            if number == 1:
-                label.append([1,0,0,0,0,0])
-            elif number == 2:
-                label.append([0,1,0,0,0,0])
-            elif number == 3:
-                label.append([0,0,1,0,0,0])
-            elif number == 4:
-                label.append([0,0,0,1,0,0])
-            elif number == 5:
-                label.append([0,0,0,0,1,0])
-            elif number == 6:
-                label.append([0,0,0,0,0,1])
-    
-    return np.array(label).astype(np.float64)
-
-
-# Function loads the label actions respective to the sensor data for the testing set.
-def load_test_label():
-
     print("Loading Testing Labels . . .")
 
     label = []
 
     with open("Dataset\\test\\y_test.txt") as f:
+           
         for row in f:
             number = int(row.strip())
+
+            
             if number == 1:
-                label.append([1,0,0,0,0,0])
+                label.append([1,0,0,0,0])
             elif number == 2:
-                label.append([0,1,0,0,0,0])
+                label.append([0,1,0,0,0])
             elif number == 3:
-                label.append([0,0,1,0,0,0])
+                label.append([0,0,1,0,0])
             elif number == 4:
-                label.append([0,0,0,1,0,0])
-            elif number == 5:
-                label.append([0,0,0,0,1,0])
-            elif number == 6:
-                label.append([0,0,0,0,0,1])
-    
-    return np.array(label).astype(np.float64)
+                label.append([0,0,0,1,0])
+            else:
+                label.append([0,0,0,0,1])
+
+    total = np.transpose([body_acc_x, body_acc_y, body_acc_z, body_gyr_x, body_gyr_y, body_gyr_z],(1,2,0)).astype(np.float64)
+    body_acc_x = np.transpose([body_acc_x],(1,2,0)).astype(np.float64)
+    body_acc_y = np.transpose([body_acc_y],(1,2,0)).astype(np.float64)
+    body_acc_z = np.transpose([body_acc_z],(1,2,0)).astype(np.float64)
+    body_gyr_x = np.transpose([body_gyr_x],(1,2,0)).astype(np.float64)
+    body_gyr_y = np.transpose([body_gyr_y],(1,2,0)).astype(np.float64)
+    body_gyr_z = np.transpose([body_gyr_z],(1,2,0)).astype(np.float64)
+
+    return body_acc_x, body_acc_y, body_acc_z, body_gyr_x, body_gyr_y, body_gyr_z, total, np.array(label).astype(np.float64)
 
 
+'''
 # Function extract mean, standard deviation, maximum value and minimum value from array
 def extract_data(body_acc_x, body_acc_y, body_acc_z, body_gyr_x, body_gyr_y, body_gyr_z):
 
@@ -258,35 +253,66 @@ def extract_data(body_acc_x, body_acc_y, body_acc_z, body_gyr_x, body_gyr_y, bod
     min_gyr_z = np.amin(body_gyr_z, axis=1).reshape(-1,1)
 
     return np.concatenate((acc_x_mean, acc_y_mean, acc_z_mean, gyr_x_mean, gyr_y_mean, gyr_z_mean, sd_acc_x, sd_acc_y, sd_acc_z, sd_gyr_x, sd_gyr_y, sd_gyr_z, max_acc_x, max_acc_y, max_acc_z, max_gyr_x, max_gyr_y, max_gyr_z, min_acc_x, min_acc_y, min_acc_z, min_gyr_x, min_gyr_y, min_gyr_z), axis=1)
-
-# Function creates the artificial neural network model for the extracted datas
-def get_ann_model_for_extracted_data():
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Dense(128, activation="relu", input_shape=(24,)),
-        tf.keras.layers.Dense(128, activation="relu"),
-        tf.keras.layers.Dense(128, activation="relu"),
-        tf.keras.layers.Dense(6, activation="softmax")
-    ])
-
-    model.compile(
-        optimizer="adam",
-        loss="categorical_crossentropy",
-        metrics=["accuracy"]
-    )
-
-    return model
+'''
 
 
 # Function creates the artificial neural network model.
 def get_ann_model():
-    
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Flatten(input_shape=(128, 6)),
-        tf.keras.layers.Dense(128, activation="relu"),
-        tf.keras.layers.Dense(128, activation="relu"),
-        tf.keras.layers.Dense(128, activation="relu"),
-        tf.keras.layers.Dense(6, activation="softmax")
-    ])
+
+    inputA = tf.keras.layers.Input(shape=(128,1))
+    inputB = tf.keras.layers.Input(shape=(128,1))
+    inputC = tf.keras.layers.Input(shape=(128,1))
+    inputD = tf.keras.layers.Input(shape=(128,1))
+    inputE = tf.keras.layers.Input(shape=(128,1))
+    inputF = tf.keras.layers.Input(shape=(128,1))
+    inputG = tf.keras.layers.Input(shape=(128,6))
+
+    x = tf.keras.layers.Flatten()(inputA)
+    x = tf.keras.layers.Dense(512, activation="relu")(x)
+    x = tf.keras.layers.Dense(128, activation="relu")(x)
+    x = tf.keras.Model(inputs=inputA, outputs=x)
+
+    y = tf.keras.layers.Flatten()(inputB)
+    y = tf.keras.layers.Dense(512, activation="relu")(y)
+    y = tf.keras.layers.Dense(128, activation="relu")(y)
+    y = tf.keras.Model(inputs=inputB, outputs=y)
+
+    z = tf.keras.layers.Flatten()(inputC)
+    z = tf.keras.layers.Dense(512, activation="relu")(z)
+    z = tf.keras.layers.Dense(128, activation="relu")(z)
+    z = tf.keras.Model(inputs=inputC, outputs=z)
+
+    g_x = tf.keras.layers.Flatten()(inputD)
+    g_x = tf.keras.layers.Dense(512, activation="relu")(g_x)
+    g_x = tf.keras.layers.Dense(128, activation="relu")(g_x)
+    g_x = tf.keras.Model(inputs=inputD, outputs=g_x)
+
+    g_y = tf.keras.layers.Flatten()(inputE)
+    g_y = tf.keras.layers.Dense(512, activation="relu")(g_y)
+    g_y = tf.keras.layers.Dense(128, activation="relu")(g_y)
+    g_y = tf.keras.Model(inputs=inputE, outputs=g_y)
+
+    g_z = tf.keras.layers.Flatten()(inputF)
+    g_z = tf.keras.layers.Dense(512, activation="relu")(g_z)
+    g_z = tf.keras.layers.Dense(128, activation="relu")(g_z)
+    g_z = tf.keras.Model(inputs=inputF, outputs=g_z)
+
+    total = tf.keras.layers.Flatten()(inputG)
+    total = tf.keras.layers.Dense(512, activation="relu")(total)
+    total = tf.keras.layers.Dense(128, activation="relu")(total)
+    total = tf.keras.Model(inputs=inputG, outputs=total)
+
+    combined = tf.keras.layers.concatenate([x.output, y.output, z.output, g_x.output, g_y.output, g_z.output, total.output])
+
+    final = tf.keras.layers.Dense(2048, activation="relu")(combined)
+    final = tf.keras.layers.Dropout(0.5)(final)
+    final = tf.keras.layers.Dense(512, activation="relu")(final)
+    final = tf.keras.layers.Dropout(0.5)(final)
+    final = tf.keras.layers.Dense(128, activation="relu")(final)
+    final = tf.keras.layers.Dropout(0.5)(final)
+    final = tf.keras.layers.Dense(5, activation="softmax")(final)
+
+    model = tf.keras.Model(inputs=[x.input, y.input, z.input, g_x.input, g_y.input, g_z.input, total.input], outputs=final)
 
     model.compile(
         optimizer="adam",
@@ -300,13 +326,68 @@ def get_ann_model():
 # Function creates the convolutional neural network model.
 def get_cnn_model():
 
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Conv1D(filters = 64, kernel_size = 3, activation = 'relu', input_shape=(128, 6)),
-        tf.keras.layers.Conv1D(filters = 64, kernel_size = 3, activation = 'relu'),
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(128, activation="relu"),
-        tf.keras.layers.Dense(6, activation="softmax")
-    ])
+    inputA = tf.keras.layers.Input(shape=(128,1))
+    inputB = tf.keras.layers.Input(shape=(128,1))
+    inputC = tf.keras.layers.Input(shape=(128,1))
+    inputD = tf.keras.layers.Input(shape=(128,1))
+    inputE = tf.keras.layers.Input(shape=(128,1))
+    inputF = tf.keras.layers.Input(shape=(128,1))
+    inputG = tf.keras.layers.Input(shape=(128,6))
+
+    x = tf.keras.layers.Conv1D(filters=24, kernel_size=3, activation="relu")(inputA)
+    x = tf.keras.layers.MaxPooling1D(pool_size=3)(x)
+    x = tf.keras.layers.Conv1D(filters=24, kernel_size=3, activation="relu")(x)
+    x = tf.keras.layers.Flatten()(x)
+    x = tf.keras.Model(inputs=inputA, outputs=x)
+
+    y = tf.keras.layers.Conv1D(filters=24, kernel_size=3, activation="relu")(inputB)
+    y = tf.keras.layers.MaxPooling1D(pool_size=3)(y)
+    y = tf.keras.layers.Conv1D(filters=24, kernel_size=3, activation="relu")(y)
+    y = tf.keras.layers.Flatten()(y)
+    y = tf.keras.Model(inputs=inputB, outputs=y)
+
+    z = tf.keras.layers.Conv1D(filters=24, kernel_size=3, activation="relu")(inputC)
+    z = tf.keras.layers.MaxPooling1D(pool_size=3)(z)
+    z = tf.keras.layers.Conv1D(filters=24, kernel_size=3, activation="relu")(z)
+    z = tf.keras.layers.Flatten()(z)
+    z = tf.keras.Model(inputs=inputC, outputs=z)
+
+    g_x = tf.keras.layers.Conv1D(filters=24, kernel_size=3, activation="relu")(inputD)
+    g_x = tf.keras.layers.MaxPooling1D(pool_size=3)(g_x)
+    g_x = tf.keras.layers.Conv1D(filters=24, kernel_size=3, activation="relu")(g_x)
+    g_x = tf.keras.layers.Flatten()(g_x)
+    g_x = tf.keras.Model(inputs=inputD, outputs=g_x)
+
+    g_y = tf.keras.layers.Conv1D(filters=24, kernel_size=3, activation="relu")(inputE)
+    g_y = tf.keras.layers.MaxPooling1D(pool_size=3)(g_y)
+    g_y = tf.keras.layers.Conv1D(filters=24, kernel_size=3, activation="relu")(g_y)
+    g_y = tf.keras.layers.Flatten()(g_y)
+    g_y = tf.keras.Model(inputs=inputE, outputs=g_y)
+
+    g_z = tf.keras.layers.Conv1D(filters=24, kernel_size=3, activation="relu")(inputF)
+    g_z = tf.keras.layers.MaxPooling1D(pool_size=3)(g_z)
+    g_z = tf.keras.layers.Conv1D(filters=24, kernel_size=3, activation="relu")(g_z)
+    g_z = tf.keras.layers.Flatten()(g_z)
+    g_z = tf.keras.Model(inputs=inputF, outputs=g_z)
+
+    total = tf.keras.layers.Conv1D(filters=24, kernel_size=3, activation="relu")(inputG)
+    total = tf.keras.layers.MaxPooling1D(pool_size=3)(total)
+    total = tf.keras.layers.Conv1D(filters=24, kernel_size=3, activation="relu")(total)
+    total = tf.keras.layers.Flatten()(total)
+    total = tf.keras.Model(inputs=inputG, outputs=total)
+
+    combined = tf.keras.layers.concatenate([x.output, y.output, z.output, g_x.output, g_y.output, g_z.output, total.output])
+
+    final = tf.keras.layers.Dense(2048, activation="relu")(combined)
+    final = tf.keras.layers.Dropout(0.5)(final)
+    final = tf.keras.layers.Dense(512, activation="relu")(final)
+    final = tf.keras.layers.Dropout(0.5)(final)
+    final = tf.keras.layers.Dense(128, activation="relu")(final)
+    final = tf.keras.layers.Dropout(0.5)(final)
+    final = tf.keras.layers.Dense(5, activation="softmax")(final)
+
+    model = tf.keras.Model(inputs=[x.input, y.input, z.input, g_x.input, g_y.input, g_z.input, total.input], outputs=final)
+    
 
     model.compile(
         optimizer="adam",
@@ -316,7 +397,7 @@ def get_cnn_model():
 
     return model
 
-
+'''
 # Function creates the Recurrent neural networks
 def get_rnn_model():
 
@@ -334,6 +415,7 @@ def get_rnn_model():
     )
 
     return model
+'''
 
 
 if __name__ == "__main__":
